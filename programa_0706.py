9d20a84cd51950a597858b1b36e36445a7d727db
TWTVtrG <163884585+TWTVtrG@users.noreply.github.com>
2024-06-14 19:18:56 -0300

    Add files via upload

---
+++ programa_0706.py
@@ -0,0 +1,32 @@
+import pandas as pd  
+import matplotlib.pyplot as plt
+import streamlit as st
+
+st.set_page_config(
+
+page_title="Empresa 1", 
+)
+
+st.header("Dados da empresa 1")
+
+arquivo = "https://raw.githubusercontent.com/WesleyInfoBr2/aula_teste/main/empresa1.csv" 
+dfe = pd.read_csv(arquivo, sep=';') 
+st.dataframe(dfe.head(3))
+
+st.write("Gráfico de linha dos indicadores ao longo do tempo")
+
+fig, ax = plt.subplots()
+dfe.plot(ax=ax)
+st.pyplot(fig)
+
+fig, ax = plt.subplots()
+dfe.plot(kind = 'scatter', x = 'EBITDA', y = 'Lucro operacional', ax=ax)
+st.pyplot(fig)
+
+fig, ax = plt.subplots()
+dfe["Lucro do período"].plot(kind = 'hist', ax=ax)
+st.pyplot(fig)
+
+
+st.write(dfe.groupby('Ano').mean())
+
