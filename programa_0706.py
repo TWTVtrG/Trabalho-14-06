8fa0d2cf5b4a48a14d47b2ddccea56f7791d5d9d
TWTVtrG <163884585+TWTVtrG@users.noreply.github.com>
2024-06-14 19:56:50 -0300

    Update programa_0706.py

--- programa_0706.py
+++ programa_0706.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-st.set_page_config((" Dados da empresa 1 ")
+st.set_page_config(("Dados da empresa 1")
 
 page_title="Empresa 1", 
 page_icon="👋"
